ba0e9d66f68c8530268dfcf42fe1deac4674ce0a
dalayach <davidalayachew@gmail.com>
2021-05-29 13:45:00 -0400

    Maybe it all needs to be in a package?

---
+++ testPackage/TestClass.java
@@ -0,0 +1,23 @@
+package testPackage;
+
+import javax.swing.JFrame;
+
+public class TestClass
+{
+
+   public static void main(String[] args)
+   {
+   
+      JFrame frame = new JFrame();
+   
+      frame.setTitle("TestClass JFrame title");
+      frame.setSize(300, 300);
+      frame.setLocation(300, 300);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+   	
+      frame.setVisible(true);
+   
+   }
+
+}
+
