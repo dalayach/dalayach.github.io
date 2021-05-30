30b5f64ee3c3b717a8f220e31394ec490d728aff
dalayach <davidalayachew@gmail.com>
2021-05-29 23:38:45 -0400

    Let's check and see which version of java we run

--- TestClass.java
+++ TestClass.java
@@ -6,7 +6,7 @@ public class TestClass
    public static void main(String[] args)
    {
    
-      JOptionPane.showMessageDialog(null, "B");
+      JOptionPane.showMessageDialog(null, System.getProperty("java.version"));
    
    }
 
